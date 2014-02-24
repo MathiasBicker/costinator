a62e477894513bdca9a054d860b207c33c27cc74
MathiasBicker <mathias.bicker@web.de>
2014-02-24 19:28:11 +0100

    adding comments

--- Costinator/src/de/bandb/costinator/BusinessAssesment.java
+++ Costinator/src/de/bandb/costinator/BusinessAssesment.java
@@ -12,15 +12,19 @@ import de.bandb.costinator.database.entities.TCostgroup;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.TextView;
 
 public class BusinessAssesment extends Activity {
 
+	public static final String LOGTAG 			= "BusinessAssesment";
 	public static final String ELEMENTLISTTAG 	= "elementlist";
 	public static final String COSTGROUPTAG 	= "costgroup";
 	public static final String DAYSTAG 			= "days";
+	//error messages
 	public static final String WRONGPERIOD 		= "wrong period";
+	public static final String EMPTYLIST 		= "elementlist is empty";
 	
 	private List<TCostelement> 	elementList;
 	private TCostgroup 			costgroup;
@@ -33,29 +37,40 @@ public class BusinessAssesment extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_business_assesment);
 		
+		//initializing widgets
 		TextView 	elements 		= (TextView) findViewById(R.id.costgroup_business_assesment_elements);
 		TextView 	costgroupView 	= (TextView) findViewById(R.id.costgroup_business_assesment_costgroup);
 		TextView 	sumView 		= (TextView) findViewById(R.id.costgroup_business_assesment_sum);
+		//getting information from intent
 		Intent 		intent 			= getIntent();
 		Bundle 		bundle 			= intent.getExtras();
 		if(bundle != null) {
 			elementList = (List<TCostelement>) bundle.get(ELEMENTLISTTAG);
 			costgroup 	= (TCostgroup) bundle.get(COSTGROUPTAG);
 			days 		= bundle.getInt(DAYSTAG);
+			//displaying name of costgroup
 			costgroupView.append(costgroup.getName());
 		}
-		if(elementList.isEmpty());
+		
+		//checking if elementlist is empty
+		if(elementList.isEmpty()) {
+			Log.e(LOGTAG, EMPTYLIST);
+			throw new RuntimeException(EMPTYLIST);
+		}
+		
+		//displaying values for each element in list
 		for(TCostelement e : elementList) {
 			sum += computeValue(e.getValue(), e.getPeriod());
 			double endValue = Math.round(100.0 * computeValue(e.getValue(), e.getPeriod())) / 100.0;	//rounding values
 			elements.append(e.getName() + ": " + endValue + " (" + e.getValue() + findPeriod(e.getPeriod()) + ")\n");
 		}
 		
-		double perDay = Math.round(100.0 * sum/days) / 100.0;			//rounding values
-		double perWeek = Math.round(100.0 * sum/days*7) / 100.0;
+		//computing sums and displaying them
+		double perDay 	= Math.round(100.0 * sum/days) / 100.0;			//rounding values
+		double perWeek 	= Math.round(100.0 * sum/days*7) / 100.0;
 		double perMonth = Math.round(100.0 * sum/days*30) / 100.0;
 		double perQuart = Math.round(100.0 * sum/days*90) / 100.0;
-		double perYear = Math.round(100.0 * sum/days*360) / 100.0;
+		double perYear 	= Math.round(100.0 * sum/days*360) / 100.0;
 		sumView.append(sum + " (" + perDay + getResources().getString(R.string.dayly) + "; " + perWeek + getResources().getString(R.string.weekly)
 					   + "; " + perMonth + getResources().getString(R.string.monthly) + "; " + perQuart + getResources().getString(R.string.quart)
 					   + "; " + perYear + getResources().getString(R.string.yearly) + ")");
@@ -85,6 +100,7 @@ public class BusinessAssesment extends Activity {
 			type = getResources().getString(R.string.yearly);
 			break;
 		default:
+			Log.e(LOGTAG, WRONGPERIOD);
 			throw new RuntimeException(WRONGPERIOD);
 		}
 		return type;
@@ -115,6 +131,7 @@ public class BusinessAssesment extends Activity {
 			computedValue = value * (days/360);
 			break;
 		default:
+			Log.e(LOGTAG, WRONGPERIOD);
 			throw new RuntimeException(WRONGPERIOD);
 		}
 		return computedValue;
