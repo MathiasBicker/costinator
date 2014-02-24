9ab70ccb247875dd13482031b37398762bd7ed0a
MathiasBicker <mathias.bicker@web.de>
2014-02-24 19:17:53 +0100

    copmuting assesment rounding values

--- Costinator/src/de/bandb/costinator/BusinessAssesment.java
+++ Costinator/src/de/bandb/costinator/BusinessAssesment.java
@@ -28,6 +28,7 @@ public class BusinessAssesment extends Activity {
 	private List<TCostelement> 	elementList;
 	private TCostgroup 			costgroup;
 	private int					days;
+	private double				sum = 0.0;
 	
 	@SuppressWarnings("unchecked")
 	@Override
@@ -35,10 +36,11 @@ public class BusinessAssesment extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_business_assesment);
 		
-		TextView 	elements = (TextView) findViewById(R.id.costgroup_business_assesment_elements);
-		TextView 	costgroupView = (TextView) findViewById(R.id.costgroup_business_assesment_costgroup);
-		Intent 		intent = getIntent();
-		Bundle 		bundle = intent.getExtras();
+		TextView 	elements 		= (TextView) findViewById(R.id.costgroup_business_assesment_elements);
+		TextView 	costgroupView 	= (TextView) findViewById(R.id.costgroup_business_assesment_costgroup);
+		TextView 	sumView 		= (TextView) findViewById(R.id.costgroup_business_assesment_sum);
+		Intent 		intent 			= getIntent();
+		Bundle 		bundle 			= intent.getExtras();
 		if(bundle != null) {
 			elementList = (List<TCostelement>) bundle.get(ELEMENTLISTTAG);
 			costgroup 	= (TCostgroup) bundle.get(COSTGROUPTAG);
@@ -46,11 +48,47 @@ public class BusinessAssesment extends Activity {
 			costgroupView.append(costgroup.getName());
 		}
 		if(elementList.isEmpty());
-		for(TCostelement e : elementList)
-			elements.append(e.getName() + ": " + e.getValue());
+		for(TCostelement e : elementList) {
+			sum += computeValue(e.getValue(), e.getPeriod());
+			double endValue = Math.round(100.0 * computeValue(e.getValue(), e.getPeriod())) / 100.0;	//rounding values
+			elements.append(e.getName() + ": " + endValue + " (" + e.getValue() + findPeriod(e.getPeriod()) + ")\n");
+		}
+		
+		double perDay = Math.round(100.0 * sum/days) / 100.0;			//rounding values
+		double perWeek = Math.round(100.0 * sum/days*7) / 100.0;
+		double perMonth = Math.round(100.0 * sum/days*30) / 100.0;
+		double perQuart = Math.round(100.0 * sum/days*90) / 100.0;
+		double perYear = Math.round(100.0 * sum/days*360) / 100.0;
+		sumView.append(sum + " (" + perDay + getResources().getString(R.string.dayly) + "; " + perWeek + getResources().getString(R.string.weekly)
+					   + "; " + perMonth + getResources().getString(R.string.monthly) + "; " + perQuart + getResources().getString(R.string.quart)
+					   + "; " + perYear + getResources().getString(R.string.yearly) + ")");
 	}
 	
-	private double computeValue(double value, int period, Date from, Date until) {
+	private String findPeriod(int period) {
+		String type = null;
+		switch(period) {
+		case TCostelement.DAYLY:
+			type = getResources().getString(R.string.dayly);
+			break;
+		case TCostelement.WEEKLY:
+			type = getResources().getString(R.string.weekly);
+			break;
+		case TCostelement.MONTHLY:
+			type = getResources().getString(R.string.monthly);
+			break;
+		case TCostelement.QUART:
+			type = getResources().getString(R.string.quart);
+			break;
+		case TCostelement.YEARLY:
+			type = getResources().getString(R.string.yearly);
+			break;
+		default:
+			throw new RuntimeException(WRONGPERIOD);
+		}
+		return type;
+	}
+	
+	private double computeValue(double value, int period) {
 		double computedValue = -1.0;
 		switch(period) {
 		case TCostelement.DAYLY:
