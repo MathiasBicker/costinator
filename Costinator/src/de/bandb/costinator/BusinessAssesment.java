b3e5d75dd681d0c426f493c7cd1b791d5214b645
MathiasBicker <mathias.bicker@web.de>
2014-02-24 19:50:06 +0100

    removing warnings

--- Costinator/src/de/bandb/costinator/BusinessAssesment.java
+++ Costinator/src/de/bandb/costinator/BusinessAssesment.java
@@ -22,10 +22,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.AdapterView.OnItemClickListener;
 
 public class BusinessAssesment extends Activity {
 
